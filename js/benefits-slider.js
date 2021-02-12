7a8646499d64ca50ce75e05b6ef348fe9a7172b8
Verta-master <vertyagina@gmail.com>
2021-02-12 14:45:44 +0600

    Markup

---
+++ js/benefits-slider.js
@@ -0,0 +1,27 @@
+var breakpoint = window.matchMedia('(max-width: 766px)');
+var newsSwiper;
+var breakpointChecker = function() {
+  if (breakpoint.matches === true) {
+    if (newsSwiper !== undefined) newsSwiper.destroy(true, true);
+    return;
+    } else if (breakpoint.matches === false) {
+      return enableSwiper();
+    }
+};
+
+var enableSwiper = function() {
+  newsSwiper = new Swiper ('.benefits__slider', {
+    pagination: {
+      el: '.benefits__pagination',
+      clickable: true,
+    },
+    simulateTouch: false,
+    preventClicks: false,
+    preventClicksPropagation: false,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+  })
+};
+
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
